f302dd52af571b3e3120afe1701538d7045d1db9
Ingraham, Daniel James (GRC-LTV0) <daniel.j.ingraham@nasa.gov>
2021-08-06 09:57:01 -0400

    WIP: Integrate AcousticMetrics.jl

--- src/AcousticAnalogies.jl
+++ src/AcousticAnalogies.jl
@@ -1,5 +1,6 @@
 module AcousticAnalogies
 
+using AcousticMetrics
 using CCBlade
 using ConcreteStructs: @concrete
 using FLOWMath: akima, linear, ksmax
@@ -14,7 +15,7 @@ export get_dradii
 include("core.jl")
 export CompactSourceElement
 export AcousticObserver, StationaryAcousticObserver, ConstVelocityAcousticObserver
-export AcousticPressure
+export F1AOutput
 export adv_time
 export f1a
 export common_obs_time!, common_obs_time
